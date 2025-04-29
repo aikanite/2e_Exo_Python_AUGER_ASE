e844afa2cb0a741a0885ba4da20204d82aab8f4e
Kévin <kevin.auger78@gmail.com>
2025-04-29 17:32:02 +0200

    Copie des fichier python

---
+++ character.py
@@ -0,0 +1,23 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class CharacterError(Exception):
+    """Base class for Character error"""
+
+
+class Character:
+    """To implement"""
+
+
+class Weapon:
+    """To implement"""
+
+
+class Warrior:
+    """To implement"""
+
+
+class Magician:
+    """To implement"""
